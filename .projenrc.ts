f711cc6443027a8c38694bb22021cc276c38ab50
lazos <lazos@lazos.me>
2025-09-06 21:24:57 +0200

    feat: contribution guide

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,3 +1,4 @@
+import { TextFile } from 'projen';
 import { JobPermission } from 'projen/lib/github/workflows-model';
 import { NodePackageManager, NpmAccess } from 'projen/lib/javascript';
 import { TypeScriptProject } from 'projen/lib/typescript';
@@ -83,6 +84,27 @@ wf.addJobs({
       },
     ],
   },
+});
+
+new TextFile(project, 'CONTRIBUTING.md', {
+  lines: [
+    '# Contributing',
+    '',
+    'Contributions are appreciated,',
+    'but keep in mind that this project aims to be a minimalistic tool,',
+    'and thus requests around extending the default suite of Behaviors and Components might be rejected.',
+    'Please consider creating an auxilliary package for those cases.',
+    '',
+    'To contribute, follow these steps:',
+    '',
+    '1.  **Fork the repository** on GitHub.',
+    '2.  **Clone your fork** to your local machine.',
+    '3.  **Create a new branch** for your changes, with descriptive names like `feature/behaviors` or `fix/helm-chart-resolution`.',
+    '4.  **Make your changes**, ensuring that your code follows the style guidelines.',
+    '5.  **Commit your changes** with a clear and concise commit message.',
+    '6.  **Push your changes** to your fork on GitHub.',
+    '7.  **Open a pull request** from your fork to the `master` branch of this repository.',
+  ],
 });
 
 project.synth();
---
+++ CONTRIBUTING.md
@@ -0,0 +1,16 @@
+# Contributing
+
+Contributions are appreciated,
+but keep in mind that this project aims to be a minimalistic tool,
+and thus requests around extending the default suite of Behaviors and Components might be rejected.
+Please consider creating an auxilliary package for those cases.
+
+To contribute, follow these steps:
+
+1.  **Fork the repository** on GitHub.
+2.  **Clone your fork** to your local machine.
+3.  **Create a new branch** for your changes, with descriptive names like `feature/behaviors` or `fix/helm-chart-resolution`.
+4.  **Make your changes**, ensuring that your code follows the style guidelines.
+5.  **Commit your changes** with a clear and concise commit message.
+6.  **Push your changes** to your fork on GitHub.
+7.  **Open a pull request** from your fork to the `master` branch of this repository.
